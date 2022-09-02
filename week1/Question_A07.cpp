51273ab01919cc214b9923b8bb0e5384bc752dc1
Afeefa Ahmad <75202156+afeefaahmad@users.noreply.github.com>
2022-09-02 17:10:27 +0530

    Create Question_A07.cpp

---
+++ week1/Question_A07.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+#include <stdio.h>
+
+int main() {
+    int k,x;
+    
+    int t;
+    cin >> t;
+    
+    while(t--){
+        cin>>k >>x;
+    cout << k*7 - x <<endl;
+  }
+  return 0;                                        
+}
