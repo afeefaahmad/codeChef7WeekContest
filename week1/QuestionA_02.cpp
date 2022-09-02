2ef2d1a35f6cdc2bcefc0d792262e7be40b4f04b
Afeefa Ahmad <75202156+afeefaahmad@users.noreply.github.com>
2022-09-02 16:31:06 +0530

    Create QuestionA_02.cpp

---
+++ week1/QuestionA_02.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+#include <stdio.h>
+
+int main() {
+    int x,y;
+    
+    int t;
+    cin >> t;
+    
+    while(t--){
+        cin>>x;
+    cin>>y;    
+    if (x>=y){
+        cout<< "Yes" << endl;
+    }
+     else {
+        cout << "No" << endl;  
+    }
+   
+  }
+  return 0;                                        
+}
