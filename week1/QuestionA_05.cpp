26af9d1da81d1a6301ba56ec881be98496df0618
Afeefa Ahmad <75202156+afeefaahmad@users.noreply.github.com>
2022-09-02 12:46:22 +0530

    Create QuestionA_05.cpp

---
+++ week1/QuestionA_05.cpp
@@ -0,0 +1,14 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+//#define endl "\n"
+
+int t, x, y, z;
+int main() {
+    cin >> t;
+    while(t--) {
+        cin >> x >> y >> z;
+        cout << x - y + z << endl;
+    }
+	return 0;
+}
